89d8e207afa310e15ddae3b206c23b2ccfc0590e
Zs <email@email.com>
2019-07-13 15:36:19 +0200

    ewrtzuio

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,13 @@ public class Main {
         System.out.println("msg2");
         System.out.println("msg2");
         System.out.println("msg2");
+        System.out.println("msg2");
+        System.out.println("msg2");
+        System.out.println("msg2");
+        System.out.println("msg2");
+        System.out.println("msg2");
+        System.out.println("msg2");
+        System.out.println("msg2");
+        System.out.println("msg2");
     }
 }
